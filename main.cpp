7011314c4efd0108e1673b03fc5f329f99016916
Yegeurina Lee <48973031+Yegeurina@users.noreply.github.com>
2021-08-23 09:18:08 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,251 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <linux/types.h>
+#include <linux/netfilter.h>		/* for NF_ACCEPT */
+#include <errno.h>
+
+#include <libnetfilter_queue/libnetfilter_queue.h>
+#include <libnet.h>
+
+#include <string.h>
+#include <regex.h>
+
+char hexURL[1000];
+int is_blocked =0;
+
+void usage() {
+    printf("syntax: netfilter_test <url>\n");
+    printf("sample: netfilter_test test.gilgil.net\n");
+}
+
+void string2hexString(char* input, char* output)
+{
+    int loop;
+    int i;
+
+    i=0;
+    loop=0;
+
+    while(input[loop] != '\0')
+    {
+        sprintf((char*)(output+i),"%02X", input[loop]);
+        loop+=1;
+        i+=2;
+    }
+    //insert NULL at the end of the output string
+    output[i++] = '\0';
+}
+
+
+void urlDetect(const u_char* packet)
+{
+    printf("urlDetect\n");
+
+    const char *regexUrl = "HOST: ";
+    regex_t regexComUrl;
+    regmatch_t matchUrl[10000];
+
+    if(regcomp(&regexComUrl,regexUrl,REG_EXTENDED))
+    {
+        fprintf(stderr,"Could not compile regular Expresion.\n");
+        exit(1);
+    }
+
+
+    if(regexec(&regexComUrl,(char *)packet,sizeof(packet),matchUrl,REG_EXTENDED)==0)
+    {
+        packet = packet+matchUrl[0].rm_eo;
+
+        if(!strncmp((char *)packet,hexURL,strlen(hexURL)))
+        {
+
+            is_blocked=1;
+        }
+    }
+    is_blocked=1;
+}
+
+void packetAnalysis(unsigned char* packet)
+{
+    struct libnet_ipv4_hdr* ip = (struct libnet_ipv4_hdr*)packet;
+    uint8_t checkTCPUDP=ip->ip_p;
+    packet+=LIBNET_IPV4_H;
+
+    if(checkTCPUDP==0x06)
+    {
+        struct libnet_tcp_hdr* tcp = (struct libnet_tcp_hdr*)packet;
+
+        //printf("tcp->dport : %d\n",ntohs(tcp->th_dport));
+
+        if(ntohs(tcp->th_dport)!=80)     return;
+
+        packet+=LIBNET_TCP_H;
+        urlDetect(packet);
+    }
+}
+
+/* returns packet id */
+static u_int32_t print_pkt (struct nfq_data *tb)
+{
+    int id = 0;
+    struct nfqnl_msg_packet_hdr *ph;
+    struct nfqnl_msg_packet_hw *hwph;
+    u_int32_t mark,ifi;
+    int ret;
+    unsigned char *data;
+
+    ph = nfq_get_msg_packet_hdr(tb);
+    if (ph) {
+        id = ntohl(ph->packet_id);
+        printf("hw_protocol=0x%04x hook=%u id=%u ",
+            ntohs(ph->hw_protocol), ph->hook, id);
+    }
+
+    hwph = nfq_get_packet_hw(tb);
+    if (hwph) {
+        int i, hlen = ntohs(hwph->hw_addrlen);
+
+        printf("hw_src_addr=");
+        for (i = 0; i < hlen-1; i++)
+            printf("%02x:", hwph->hw_addr[i]);
+        printf("%02x ", hwph->hw_addr[hlen-1]);
+    }
+
+    mark = nfq_get_nfmark(tb);
+    if (mark)
+        printf("mark=%u ", mark);
+
+    ifi = nfq_get_indev(tb);
+    if (ifi)
+        printf("indev=%u ", ifi);
+
+    ifi = nfq_get_outdev(tb);
+    if (ifi)
+        printf("outdev=%u ", ifi);
+    ifi = nfq_get_physindev(tb);
+    if (ifi)
+        printf("physindev=%u ", ifi);
+
+    ifi = nfq_get_physoutdev(tb);
+    if (ifi)
+        printf("physoutdev=%u ", ifi);
+
+    ret = nfq_get_payload(tb, &data);
+    if (ret >= 0)
+    {
+        printf("payload_len=%d\n", ret);
+        is_blocked = 0;
+        packetAnalysis(data);
+    }
+
+    fputc('\n', stdout);
+
+    return id;
+}
+
+
+static int cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg,
+          struct nfq_data *nfa, void *data)
+{
+    u_int32_t id = print_pkt(nfa);
+    printf("entering callback\n");
+    if (is_blocked)
+        return nfq_set_verdict(qh, id, NF_DROP, 0, NULL);
+    else
+        return nfq_set_verdict(qh, id, NF_ACCEPT, 0, NULL);
+}
+
+int main(int argc, char **argv)
+{
+    struct nfq_handle *h;
+    struct nfq_q_handle *qh;
+    struct nfnl_handle *nh;
+    int fd;
+    int rv;
+    char buf[4096] __attribute__ ((aligned));
+
+
+
+    if(argc!=2)
+    {
+        usage();
+        exit(1);
+    }
+    char URL[strlen(argv[1])];
+    strcpy(URL, argv[1]);
+    int len = strlen(URL);
+    string2hexString(URL,hexURL);
+    //printf("%s\n",hexURL);
+
+    printf("opening library handle\n");
+    h = nfq_open();
+    if (!h) {
+        fprintf(stderr, "error during nfq_open()\n");
+        exit(1);
+    }
+
+    printf("unbinding existing nf_queue handler for AF_INET (if any)\n");
+    if (nfq_unbind_pf(h, AF_INET) < 0) {
+        fprintf(stderr, "error during nfq_unbind_pf()\n");
+        exit(1);
+    }
+
+    printf("binding nfnetlink_queue as nf_queue handler for AF_INET\n");
+    if (nfq_bind_pf(h, AF_INET) < 0) {
+        fprintf(stderr, "error during nfq_bind_pf()\n");
+        exit(1);
+    }
+
+    printf("binding this socket to queue '0'\n");
+    qh = nfq_create_queue(h,  0, &cb, NULL);
+    if (!qh) {
+        fprintf(stderr, "error during nfq_create_queue()\n");
+        exit(1);
+    }
+
+    printf("setting copy_packet mode\n");
+    if (nfq_set_mode(qh, NFQNL_COPY_PACKET, 0xffff) < 0) {
+        fprintf(stderr, "can't set packet_copy mode\n");
+        exit(1);
+    }
+
+    fd = nfq_fd(h);
+
+    for (;;) {
+        if ((rv = recv(fd, buf, sizeof(buf), 0)) >= 0) {
+            printf("pkt received\n");
+            nfq_handle_packet(h, buf, rv);
+            continue;
+        }
+        /* if your application is too slow to digest the packets that
+         * are sent from kernel-space, the socket buffer that we use
+         * to enqueue packets may fill up returning ENOBUFS. Depending
+         * on your application, this error may be ignored. nfq_nlmsg_verdict_putPlease, see
+         * the doxygen documentation of this library on how to improve
+         * this situation.
+         */
+        if (rv < 0 && errno == ENOBUFS) {
+            printf("losing packets!\n");
+            continue;
+        }
+        perror("recv failed");
+        break;
+    }
+
+    printf("unbinding from queue 0\n");
+    nfq_destroy_queue(qh);
+
+#ifdef INSANE
+    /* normally, applications SHOULD NOT issue this command, since
+     * it detaches other programs/sockets from AF_INET, too ! */
+    printf("unbinding from AF_INET\n");
+    nfq_unbind_pf(h, AF_INET);
+#endif
+
+    printf("closing library handle\n");
+    nfq_close(h);
+
+    exit(0);
+}
